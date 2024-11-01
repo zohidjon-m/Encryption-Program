dbc2b154a3c26d22a05023986877c9cbf648c79c
zohidjon-m <148512253+zohidjon-m@users.noreply.github.com>
2024-11-02 08:33:03 +0900

    add encryp and decrypt function

--- EncryptionProgram.java
+++ EncryptionProgram.java
@@ -9,7 +9,8 @@ public class EncryptionProgram {
     private char character;
     private String line;
     private char[] letters;
-    private char[] secretLetters;
+    private char[] secreteLetters;
+
     EncryptionProgram(){
          scanner = new Scanner(System.in);
          random = new Random();
@@ -39,7 +40,7 @@ public class EncryptionProgram {
                     encrypt();
                     break;
                 case 'D':
-                    decrypt();
+                    decrypt(secreteLetters);
                     break;
                 case 'Q':
                     quit();
@@ -80,13 +81,46 @@ public class EncryptionProgram {
 
     }
     private void encrypt(){
+        System.out.println("Enter a message to be encrypted: ");
+        String message = scanner.nextLine();
 
+        letters = message.toCharArray();
+
+        for(int i = 0; i<letters.length; i++){
+                for(int j = 0; j<list.size(); j++){
+                    if(letters[i]==list.get(j)){
+                        letters[i]=shuffledList.get(j);
+                        break;
+                    }
+                }
+        }
+        System.out.println("Encrypted message: ");
+        for(char x : letters){
+            System.out.print(x);
+        }
+        secreteLetters = letters;
+        System.out.println();
     }
-    private void decrypt(){
+    private void decrypt(char[] secreteLetters){
+
+        for(int i = 0; i<secreteLetters.length; i++){
+            for(int j = 0; j<shuffledList.size(); j++){
+                if(secreteLetters[i]==shuffledList.get(j)){
+                    secreteLetters[i]=list.get(j);
+                    break;
+                }
+            }
+        }
+        System.out.println("Decrypted message: ");
+        for(char x : secreteLetters){
+            System.out.print(x);
+        }
+        System.out.println();
 
     }
     private void quit(){
-
+        System.out.println("Thank you, have a nice day bro. Bye!");
+        System.exit(0);
     }
 
 }
